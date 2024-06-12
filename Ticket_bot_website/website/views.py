4c0dab7eba6e8920a6dadf857ff69027c3d4ea0a
Ken <ken205115331@gmail.com>
2024-06-12 12:27:16 +0800

    add THSR interface

--- Ticket_bot_website/website/views.py
+++ Ticket_bot_website/website/views.py
@@ -16,7 +16,8 @@ def NDHU_ticket():
         'username': request.form.get('username'), 
         'password': request.form.get('password'), 
         'rentType': request.form.get('rentType'), 
-        'court': request.form.get('court'), 
+        'court': request.form.get('court'),
+        'date': request.form.get('date').replace("-", "/"), 
         'timeSelection': request.form.get('timeSelection'),
         'customTime': request.form.get('customTime')}
 
@@ -25,6 +26,29 @@ def NDHU_ticket():
 
     return render_template("NDHUTicket.html")
 
-@views.route('/THSRTicket', methods=['GET'])
+@views.route('/THSRTicket', methods=['GET', 'POST'])
 def THSR_ticket():
-    return render_template("home.html")
+    if request.method == 'POST':
+        user_info = {
+        'ID': request.form.get('ID'), 
+        'startStation': request.form.get('startStation'), 
+        'destinationStation': request.form.get('destinationStation'),
+        'typesofticket': request.form.get('typesofticket'), 
+        'typesofCar': request.form.get('typesofCar'),
+        'date': request.form.get('date').replace("-", "/"),
+        'time': request.form.get('time'),
+        'fare': {
+            "fullFare": request.form.get('fullFare'), 
+            "halfFare": request.form.get('halfFare'),
+            "disabledFare": request.form.get('disabledFare'),
+            "seniorFare": request.form.get('seniorFare'),
+            "studentFare": request.form.get('studentFare'),
+            },
+        'ticketTypeSelection': request.form.get('ticketTypeSelection'),
+        'customTicket': request.form.get('customTicket'),
+        }
+
+        print(user_info)
+        return redirect(url_for('views.THSR_ticket'))
+
+    return render_template("THSRTicket.html")
